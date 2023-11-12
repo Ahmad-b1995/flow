6123bb25125be2b5a29c0181f13c8dfb920a942c
ahmad baghereslami <ahmad.b1995@gmail.com>
2023-11-12 10:10:50 +0330

    routes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Home() {
   return (
     <>
-    
+      <Link to="/board">Board</Link>
     </>
   );
 }
