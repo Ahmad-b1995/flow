c4c6c55e83f3ba7f9850efc78e0e63007eaab8b1
ahmad baghereslami <ahmad.b1995@gmail.com>
2023-12-06 20:01:56 +0330

    route fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@atlaskit/css-reset";
 import App from "./App";
-import './styles/Global.scss'
-import { BrowserRouter } from "react-router-dom";
-
+import "./styles/Global.scss";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
