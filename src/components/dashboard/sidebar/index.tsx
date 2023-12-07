305778f48e63ab168ae838d12c4ca3ed40d09ae6
ahmad baghereslami <ahmad.b1995@gmail.com>
2023-12-07 15:00:29 +0330

    dashboard - projects

--- src/components/dashboard/sidebar/index.tsx
+++ src/components/dashboard/sidebar/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import {useLocation} from "react-router-dom";
+const menuItems = ["Projects"];
 
 export default function Sidebar() {
+  const location = useLocation();
+  console.log(location.pathname.slice());
+
   return (
-    <div className="bg-gray-100 border-r-2 h-screen w-64 p-14">
-      i am sidebar
+    <div className="bg-gray-100 border-r-2 h-screen w-64 py-11 px-5">
+      <ul>
+        {menuItems.map((item) => (
+          <li
+            className={`
+            hover:bg-gray-200 cursor-pointer p-3 rounded-sm
+              ${location.pathname.includes(item.toLowerCase()) && "bg-gray-200"}
+            `}
+          >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
