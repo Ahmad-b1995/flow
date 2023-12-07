7e0e06de660bf2f5f5ebf3728380a6848895c1bc
ahmad baghereslami <ahmad.b1995@gmail.com>
2023-12-07 12:50:41 +0330

    registration

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,24 +1,21 @@
 import {FormEvent, useState} from "react";
 import {Link} from "react-router-dom";
 import Logo from "../../assets/images/logo";
-import {object, string, ValidationError} from "yup";
-
-// interface Props {
-  // onSubmit: (email: string, fullName: string, password: string) => void;
-// }
+import {object, ref, string, ValidationError} from "yup";
+import {signup} from "../../services/http/user.http.service";
 
 export default function RegisterForm() {
   const [error, setError] = useState("");
   const [formData, setFormData] = useState({
-    username: "",
     email: "",
     password: "",
+    passwordConfirm: "",
   });
 
   const schema = object().shape({
-    username: string().required(),
     email: string().required().email(),
     password: string().required().min(8),
+    passwordConfirm: string().oneOf([ref("password")], "Passwords must match"),
   });
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
@@ -27,7 +24,12 @@ export default function RegisterForm() {
       .validate(formData)
       .then((data) => {
         setError("");
-        console.log(data);
+        // console.log(data);
+        signup({
+          email: data.email,
+          password: data.password,
+          passwordConfirm: data.passwordConfirm!,
+        });
       })
       .catch((e: ValidationError) => {
         setError(e.message);
@@ -54,26 +56,6 @@ export default function RegisterForm() {
               </h2>
             </div>
             <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 "
-                >
-                  Username
-                </label>
-                <input
-                  onChange={(e) =>
-                    setFormData({...formData, username: e.target.value})
-                  }
-                  autoComplete="name"
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="bg-white border rounded-sm border-gray-200 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  placeholder="your name"
-                  required
-                />
-              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -114,13 +96,31 @@ export default function RegisterForm() {
                   required
                 />
               </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  autoComplete="new-password"
+                  onChange={(e) =>
+                    setFormData({...formData, passwordConfirm: e.target.value})
+                  }
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-white border rounded-sm border-gray-200 text-gray-900 sm:text-sm  focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
+                />
+              </div>
               <div className="flex flex-col gap-5 items-center justify-center">
                 <button
                   type="submit"
-                  className="w-full  disabled:text-gray-400 disabled:bg-gray-300 text-white mt-9 bg-main hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-md text-sm px-5 py-2.5 text-center "
-                  disabled={
-                    !formData.username || !formData.email || !formData.password
-                  }
+                  className="w-full disabled:text-gray-400 disabled:bg-gray-300 text-white mt-9 bg-main hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-md text-sm px-5 py-2.5 text-center "
+                  disabled={!formData.email || !formData.password}
                 >
                   Sign up
                 </button>
@@ -129,8 +129,8 @@ export default function RegisterForm() {
                   <p className="text-sm ">
                     Have an account?{" "}
                     <Link
-                      to={"/login"}
-                      className=" text-green-700 font-semibold text-primary-600 hover:underline"
+                      to={"/signin"}
+                      className=" text-main font-semibold text-primary-600 hover:underline"
                     >
                       Sign in
                     </Link>
--- src/services/http/user.http.service.ts
+++ src/services/http/user.http.service.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-
-// axios.get()
+export const signup = (data: {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+}) => {
+  return axios.post(`${process.env.REACT_APP_API_URL!}/auth/sign-up`, data);
+};
